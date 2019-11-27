e77836e79eb212483452aa64d01bc9394484d6c9
judesama <jude.agagad@boom.camp>
2019-11-27 16:50:14 +0800

    Update chat module

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Paper, Divider } from '@material-ui/core';
 import ChatList from './ChatList';
 import ReplyForm from './ReplyForm';
+import { getData } from '../../utils/api';
 export default function Chat() {
+	const [ chatData, setChatData ] = useState([]);
+	const messagesEndRef = useRef(null);
+
+	useEffect(() => {
+		getData('/chats').then((result) => {
+			setChatData(result.data);
+		});
+	});
+
+	function scrollToBottom() {
+		if (messagesEndRef.current) {
+			messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+		}
+	}
+
 	return (
 		<React.Fragment>
 			<Paper square={true} className="animated bounceInRight chat-wrapper">
@@ -10,9 +26,9 @@ export default function Chat() {
 					<span className="chat-title">Online : 20</span>
 				</div>
 				<Divider />
-				<ChatList />
+				<ChatList data={chatData} messagesEndRef={messagesEndRef} scrollToBottom={scrollToBottom} />
 			</Paper>
-			<ReplyForm />
+			<ReplyForm scrollToBottom={scrollToBottom} />
 		</React.Fragment>
 	);
 }
--- src/components/Chat/ChatList.jsx
+++ src/components/Chat/ChatList.jsx
@@ -1,107 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
 
-export default function ChatList() {
+export default function ChatList(props) {
 	return (
 		<List className="chat-list-container">
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Labore ex ipsum nostrud velit officia dolor qui. Adipisicing do esse pariatur ad sunt.Sint pariatur commodo ut et laborum proident adipisicing aliquip aliquip dolore.Sunt eu est ipsum dolor consequat sint in voluptate reprehenderit deserunt qui.Aute ad et in dolore ipsum mollit ea nisi.Minim consectetur laboris culpa ex qui labore nostrud esse velit est anim nulla eiusmod ea.Laborum ad magna occaecat ad id duis nisi commodo deserunt tempor."
-				/>
-			</ListItem>
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Duis reprehenderit reprehenderit aliqua in mollit irure proident.Dolor excepteur ex minim incididunt ea irure exercitation sint proident excepteur qui.Consequat excepteur esse quis do ex irure.Exercitation sit dolore consectetur eiusmod voluptate nisi eiusmod. Sunt excepteur Lorem pariatur irure anim pariatur id voluptate consequat tempor dolore minim.Excepteur dolore excepteur consequat Lorem qui nulla ea.Velit elit tempor deserunt ad.Qui mollit enim duis non officia exercitation incididunt amet reprehenderit.Sit sunt non laboris eu aliqua non cillum nisi.Sint aliqua anim excepteur sit.Ad anim quis tempor ut Lorem incididunt excepteur.Qui id adipisicing excepteur sint irure eiusmod sint proident ex."
-				/>
-			</ListItem>
+			{props.data.map((chat) => (
+				<ListItem className="chat-list-item" key={chat.id}>
+					<ListItemAvatar>
+						<Avatar src={chat.image} />
+					</ListItemAvatar>
+					<ListItemText primary={chat.email} secondary={chat.message} />
+				</ListItem>
+			))}
 
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Duis reprehenderit reprehenderit aliqua in mollit irure proident.Dolor excepteur ex minim incididunt ea irure exercitation sint proident excepteur qui.Consequat excepteur esse quis do ex irure.Exercitation sit dolore consectetur eiusmod voluptate nisi eiusmod. Sunt excepteur Lorem pariatur irure anim pariatur id voluptate consequat tempor dolore minim.Excepteur dolore excepteur consequat Lorem qui nulla ea.Velit elit tempor deserunt ad.Qui mollit enim duis non officia exercitation incididunt amet reprehenderit.Sit sunt non laboris eu aliqua non cillum nisi.Sint aliqua anim excepteur sit.Ad anim quis tempor ut Lorem incididunt excepteur.Qui id adipisicing excepteur sint irure eiusmod sint proident ex."
-				/>
-			</ListItem>
-
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Duis reprehenderit reprehenderit aliqua in mollit irure proident.Dolor excepteur ex minim incididunt ea irure exercitation sint proident excepteur qui.Consequat excepteur esse quis do ex irure.Exercitation sit dolore consectetur eiusmod voluptate nisi eiusmod. Sunt excepteur Lorem pariatur irure anim pariatur id voluptate consequat tempor dolore minim.Excepteur dolore excepteur consequat Lorem qui nulla ea.Velit elit tempor deserunt ad.Qui mollit enim duis non officia exercitation incididunt amet reprehenderit.Sit sunt non laboris eu aliqua non cillum nisi.Sint aliqua anim excepteur sit.Ad anim quis tempor ut Lorem incididunt excepteur.Qui id adipisicing excepteur sint irure eiusmod sint proident ex."
-				/>
-			</ListItem>
-
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Duis reprehenderit reprehenderit aliqua in mollit irure proident.Dolor excepteur ex minim incididunt ea irure exercitation sint proident excepteur qui.Consequat excepteur esse quis do ex irure.Exercitation sit dolore consectetur eiusmod voluptate nisi eiusmod. Sunt excepteur Lorem pariatur irure anim pariatur id voluptate consequat tempor dolore minim.Excepteur dolore excepteur consequat Lorem qui nulla ea.Velit elit tempor deserunt ad.Qui mollit enim duis non officia exercitation incididunt amet reprehenderit.Sit sunt non laboris eu aliqua non cillum nisi.Sint aliqua anim excepteur sit.Ad anim quis tempor ut Lorem incididunt excepteur.Qui id adipisicing excepteur sint irure eiusmod sint proident ex."
-				/>
-			</ListItem>
-
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Duis reprehenderit reprehenderit aliqua in mollit irure proident.Dolor excepteur ex minim incididunt ea irure exercitation sint proident excepteur qui.Consequat excepteur esse quis do ex irure.Exercitation sit dolore consectetur eiusmod voluptate nisi eiusmod. Sunt excepteur Lorem pariatur irure anim pariatur id voluptate consequat tempor dolore minim.Excepteur dolore excepteur consequat Lorem qui nulla ea.Velit elit tempor deserunt ad.Qui mollit enim duis non officia exercitation incididunt amet reprehenderit.Sit sunt non laboris eu aliqua non cillum nisi.Sint aliqua anim excepteur sit.Ad anim quis tempor ut Lorem incididunt excepteur.Qui id adipisicing excepteur sint irure eiusmod sint proident ex."
-				/>
-			</ListItem>
-
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Duis reprehenderit reprehenderit aliqua in mollit irure proident.Dolor excepteur ex minim incididunt ea irure exercitation sint proident excepteur qui.Consequat excepteur esse quis do ex irure.Exercitation sit dolore consectetur eiusmod voluptate nisi eiusmod. Sunt excepteur Lorem pariatur irure anim pariatur id voluptate consequat tempor dolore minim.Excepteur dolore excepteur consequat Lorem qui nulla ea.Velit elit tempor deserunt ad.Qui mollit enim duis non officia exercitation incididunt amet reprehenderit.Sit sunt non laboris eu aliqua non cillum nisi.Sint aliqua anim excepteur sit.Ad anim quis tempor ut Lorem incididunt excepteur.Qui id adipisicing excepteur sint irure eiusmod sint proident ex."
-				/>
-			</ListItem>
-			<ListItem className="chat-list-item">
-				<ListItemAvatar>
-					<Avatar>
-						<ImageIcon />
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText
-					primary="Jude-sama"
-					secondary="Duis reprehenderit reprehenderit aliqua in mollit irure proident.Dolor excepteur ex minim incididunt ea irure exercitation sint proident excepteur qui.Consequat excepteur esse quis do ex irure.Exercitation sit dolore consectetur eiusmod voluptate nisi eiusmod. Sunt excepteur Lorem pariatur irure anim pariatur id voluptate consequat tempor dolore minim.Excepteur dolore excepteur consequat Lorem qui nulla ea.Velit elit tempor deserunt ad.Qui mollit enim duis non officia exercitation incididunt amet reprehenderit.Sit sunt non laboris eu aliqua non cillum nisi.Sint aliqua anim excepteur sit.Ad anim quis tempor ut Lorem incididunt excepteur.Qui id adipisicing excepteur sint irure eiusmod sint proident ex."
-				/>
-			</ListItem>
+			<div ref={props.messagesEndRef} />
 		</List>
 	);
 }
